581bdd1e6348d6093456d96e40d7acd9bcf8de3f
Naveed-dev30 <Naveed.dev30@gmail.com>
2022-12-24 14:21:13 +0500

    more file paths added

--- lib/app/const/file_path_consts.dart
+++ lib/app/const/file_path_consts.dart
@@ -4,11 +4,30 @@ class FilePath {
   FilePath._internal();
 
   ///Env Files
-  static String ENV_PATH = 'env/.env';
+  static String get ENV_PATH => 'env/.env';
 
   ///Icons
-  static String APP_ICON = 'assets/icons/app_icon/chat.png';
+  static String get APP_ICON => 'assets/icons/app_icon/chat.png';
 
   ///Images
-  static String SPLASH_SCREEN_IMAGE = 'assets/images/splash/splash_image.jpg';
+  static String get SPLASH_SCREEN_IMAGE => 'assets/images/splash/splash_image.jpg';
+
+  ///SVG Pictures
+  static String get BUDDIES_SVG_PICTURE => 'assets/svgs/onboard/buddies.svg';
+
+  ///Onboard Pictures
+  static String get ONBOARD_1 => 'assets/svgs/onboard/onboard_1.svg';
+
+  static String get ONBOARD_2 => 'assets/svgs/onboard/onboard_2.svg';
+
+  static String get ONBOARD_3 => 'assets/svgs/onboard/onboard_3.svg';
+
+  ///Groups
+
+  ///OnboardImages Group
+  static List<String> get onboardImages => [
+        ONBOARD_1,
+        ONBOARD_2,
+        ONBOARD_3,
+      ];
 }
