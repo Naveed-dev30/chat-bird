9929a84b005005c3826cbc1386abf67f58072f25
Naveed-dev30 <Naveed.dev30@gmail.com>
2022-12-25 21:52:35 +0500

    Update

--- test/unit_test/app/features/splash/data/repository/splash_repository_test.dart
+++ test/unit_test/app/features/splash/data/repository/splash_repository_test.dart
@@ -3,6 +3,8 @@ import 'package:chat_bird/app/exceptions/persistance_exceptions.dart';
 import 'package:chat_bird/app/features/splash/data/data_store/local/splash_local_data_store.dart';
 import 'package:chat_bird/app/features/splash/data/repository/splash_repository.dart';
 import 'package:chat_bird/app/features/splash/data/repository/splash_repository_imp.dart';
+import 'package:chat_bird/app/helpers/remote_db_helper/remote_db_helper.dart';
+import 'package:chat_bird/app/helpers/remote_db_helper/remote_db_helper_imp.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -12,11 +14,16 @@ void main() {
   late final SplashRepository repository;
   late final SplashLocalDataStore splashLocalDataStore;
   late final NoRemoteDataStore noRemoteDataStore;
+  late final RemoteDBHelper remoteDBHelper;
 
   setUpAll(() {
+    remoteDBHelper = FirebaseDBHelperImpl();
     splashLocalDataStore = MockSplashLocalDataStore();
-    noRemoteDataStore = NoRemoteDataStore();
-    repository = SplashRepositoryImp(splashLocalDataStore, noRemoteDataStore);
+    noRemoteDataStore = NoRemoteDataStore(remoteDBHelper);
+    repository = SplashRepositoryImp(
+      splashLocalDataStore,
+      noRemoteDataStore,
+    );
   });
 
   test('IsOnboardSeen is false, when returned false from local datastore', () async {
