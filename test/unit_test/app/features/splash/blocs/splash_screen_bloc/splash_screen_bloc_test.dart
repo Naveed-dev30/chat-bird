4646d07bceb1ac09c9ee98b9b519b90cae6b6ce9
Naveed-dev30 <Naveed.dev30@gmail.com>
2022-12-25 13:27:26 +0500

    bloc test added

--- test/unit_test/app/features/splash/blocs/splash_screen_bloc/splash_screen_bloc_test.dart
+++ test/unit_test/app/features/splash/blocs/splash_screen_bloc/splash_screen_bloc_test.dart
@@ -2,24 +2,52 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:chat_bird/app/features/splash/blocs/splash_screen_bloc/splash_screen_bloc.dart';
 import 'package:chat_bird/app/features/splash/blocs/splash_screen_bloc/splash_screen_bloc_events.dart';
 import 'package:chat_bird/app/features/splash/blocs/splash_screen_bloc/splash_screen_bloc_states.dart';
+import 'package:chat_bird/app/features/splash/data/repository/splash_repository.dart';
+import 'package:chat_bird/app/features/splash/data/usecases/is_onboard_seen_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../../../mocks/repository/splash_repository/splash_repository_mock.mocks.dart';
 
 void main() {
-  SplashScreenBloc buildBloc() {
-    return SplashScreenBloc();
-  }
+  late final IsOnboardSeenUsecase isOnboardSeenUsecase;
+  late final SplashRepository splashRepository;
 
-  test('InitalState of bloc should be SplashScreenBlocInitialState', () {
-    final bloc = buildBloc();
-
-    expect(bloc.state.runtimeType, SplashScreenBlocInitialState);
+  setUpAll(() {
+    splashRepository = MockSplashRepository();
+    isOnboardSeenUsecase = IsOnboardSeenUsecase(splashRepository);
   });
 
-  blocTest(
-    'When SplashScreenAppearedBlocEvent is added, after 3 seconds SplashScreenBlocMoveToNextScreenState should emitted',
-    build: () => buildBloc(),
-    act: (bloc) => bloc.add(SplashScreenAppearedBlocEvent()),
-    expect: () => [SplashScreenBlocMoveToNextScreenState()],
-    wait: const Duration(seconds: 3),
-  );
+  SplashScreenBloc buildBloc() {
+    return SplashScreenBloc(isOnboardSeenUsecase);
+  }
+
+  group('Splash Bloc Tests', () {
+    test('InitalState of bloc should be SplashScreenBlocInitialState', () {
+      final bloc = buildBloc();
+
+      expect(bloc.state.runtimeType, SplashScreenBlocInitialState);
+    });
+    blocTest(
+      'When SplashScreenAppearedBlocEvent is added, after 3 seconds check if onboard not seen, SplashScreenBlocMoveToOnboardState should emitted',
+      build: () => buildBloc(),
+      act: (bloc) {
+        when(splashRepository.isOnboardSeen()).thenAnswer((realInvocation) async => false);
+        bloc.add(SplashScreenAppearedBlocEvent());
+      },
+      expect: () => [SplashScreenBlocMoveToOnboardState()],
+      wait: const Duration(seconds: 3),
+    );
+
+    blocTest(
+      'When SplashScreenAppearedBlocEvent is added, after 3 seconds check if onboard not seen, SplashScreenBlocMoveToOnboardState should emitted',
+      build: () => buildBloc(),
+      act: (bloc) {
+        when(splashRepository.isOnboardSeen()).thenAnswer((realInvocation) async => true);
+        bloc.add(SplashScreenAppearedBlocEvent());
+      },
+      expect: () => [SplashScreenBlocMoveToAuthState()],
+      wait: const Duration(seconds: 3),
+    );
+  });
 }
