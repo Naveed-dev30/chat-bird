ab14f3352c98c68c94a788e2f8be9ced0f20373e
Naveed-dev30 <Naveed.dev30@gmail.com>
2022-12-25 13:27:18 +0500

    Splash repository mock created

---
+++ test/mocks/repository/splash_repository/splash_repository_mock.dart
@@ -0,0 +1,4 @@
+@GenerateNiceMocks([MockSpec<SplashRepository>()])
+import 'package:chat_bird/app/features/splash/data/repository/splash_repository.dart';
+
+import 'package:mockito/annotations.dart';
